c6abca798e87c2cbb3172e8846bce4c39e7cbd1e
Jeffrey Brusseau <bobajeff82@gmail.com>
2021-01-27 14:00:40 -0600

    get pages from chromiumoxide

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
 use full_page_text_history_search::get_ws_url;
 
+use futures::StreamExt;
+use chromiumoxide::browser::Browser;
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let debug_ws_url = get_ws_url().await?;
-    println!("{}", debug_ws_url);
+    // println!("{}", debug_ws_url);
+    let (browser, mut handler) = Browser::connect(debug_ws_url).await?;
+
+    let handle = async_std::task::spawn(async move {
+        loop {
+            handler.next().await;
+        }
+    });
+
+    let mut events = browser.target_changed_listener().await?;
+    while let Some(event) = events.next().await {
+        if event.target_info.r#type == "page" {
+            let page = browser.get_page(event.target_info.target_id.clone()).await;
+            match page {
+                Ok(page) => {
+                    let title = page.get_title().await?;
+                    match title {
+                        Some(title) => {
+                            println!("{}", title);
+                        },
+                        None => {},
+                    }
+                },
+                Err(e) => {println!("{}", e)}
+            }
+            
+        }
+    }
+
+    handle.await;
     Ok(())
 }
