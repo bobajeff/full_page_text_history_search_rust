82791b972b3adadc06e991de8905d421f8d55f7a
Jeffrey Brusseau <bobajeff82@gmail.com>
2021-01-27 13:45:24 -0600

    moved get_ws_url to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
-use std::collections::HashMap;
+use full_page_text_history_search::get_ws_url;
 
-pub async fn get_ws_url() -> Result<std::string::String, Box<dyn std::error::Error>> {
-    let web_socket_debugger_url = std::thread::spawn(|| {//Keep tokio from panicing if run from a existing tokio runtime
-        let web_socket_debugger_url = tokio::runtime::Runtime::new() //Keep reqwest from panicking if run without a tokio runtime
-            .unwrap()
-            .block_on(async { wrap_in_result_function().await.unwrap_or_default() });
-            web_socket_debugger_url
-    }).join().expect("Thread panicked");
-    Ok(web_socket_debugger_url)
-}
-
-async fn wrap_in_result_function() -> Result<std::string::String, Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://127.0.0.1:9222/json/version")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    let web_socket_debugger_url = resp["webSocketDebuggerUrl"].clone();
-    Ok(web_socket_debugger_url)
-}
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let debug_ws_url = get_ws_url().await?;
